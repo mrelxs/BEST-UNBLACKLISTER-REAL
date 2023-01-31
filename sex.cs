b00cc4865cf9e95b28bc1fb59c1dca0f7b1062b7
mrelxs <admin@condogames.org>
2023-01-31 20:38:18 +0000

    Create sex.cs

---
+++ sex.cs
@@ -0,0 +1,12 @@
+using; system
+
+namespace cool program
+{
+    static void main(string[] args)
+    {
+        console.title = "dremybull simulator 2019";
+        Console.WriteLine("im bata KAMMMMMM AUGHHH!");
+        console.readkey();
+    }
+    
+}
